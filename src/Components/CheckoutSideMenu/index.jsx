ecb4a77fffa8dda1b4876f45d0a04d368e8b9d62
Rudys Natanael Acosta Crousset <ncrousset926@gmail.com>
2024-03-19 20:18:02 -0400

    Estilod e checkout

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react'
 import { Link } from 'react-router-dom'
-import { XMarkIcon, ArrowLeftIcon } from '@heroicons/react/24/outline'
+import { ArrowLeftIcon } from '@heroicons/react/24/outline'
 import { ShoppingCartContext } from '../../Context'
 import OrderCard from '../OrderCard'
 import { totalPrice } from '../../utils'
@@ -12,7 +12,7 @@ const CheckoutSideMenu = () => {
     const handleDelete = (id) => {
         const newCartProducts = context.cartProducts.filter(product => product.id !== id)
         context.setCount(context.count - 1)
-        context.setCartProducts(newCartProducts)
+        context.setCartProducts(newCartProducts) 
     }
 
     const handleChange = (id, value) => {
@@ -44,10 +44,11 @@ const CheckoutSideMenu = () => {
         context.closeCheckoutSideMenu()
     }
 
+
     return (
         <aside 
-            className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu  flex flex-col z-10 fixed right-0 border border-black bg-white`}>
-            <div className="flex justify-between items-center p-6 border-b">
+            className={`${context.isCheckoutSideMenuOpen ? 'flex' : 'hidden'} checkout-side-menu  flex flex-col z-10 fixed right-0 border border-gray bg-white`}>
+            <div className="flex justify-between items-center p-6">
                 <div>
                     <ArrowLeftIcon
                         onClick={() => context.closeCheckoutSideMenu()} 
@@ -56,23 +57,25 @@ const CheckoutSideMenu = () => {
                 <h2 className="font-medium text-xl text-gray-800">My Order</h2>
                 
             </div>
-            <div className='px-6 overflow-y-scroll flex-1'>
+            <div className='px-6 overflow-y-scroll flex-1 border-y'>
             {
                 context.cartProducts.map(product => (
                     <OrderCard
                         id={product.id}  
-                        handleDelete={ handleDelete}
+                        handleDelete={handleDelete}
                         handleChange={handleChange}
                         key={product.id}
                         title={product.title} 
                         price={product.price} 
                         imageUrl={product.images[0]}
-                        quantity={product.quantity} />
+                        quantity={product.quantity}
+                        category={product.category.name}
+                        />
                 ))
             }
 
             </div>
-            <div className='px-6 pb-6'>
+            <div className='px-6 py-6'>
                 <p className='flex justify-between items-center mb-2'>
                     <span className='text-gray-800'>Total:</span>
                     <span className='font-medium text-2xl'>${totalPrice(context.cartProducts)}</span>
@@ -80,7 +83,7 @@ const CheckoutSideMenu = () => {
 
                 <Link to='/my-orders/last'>
                     <button
-                        className='w-full py-2 mt-4 bg-black text-white rounded-lg' 
+                        className='w-full py-2 mt-4 bg-black text-white rounded-sm' 
                         onClick={() => handleCheckout() }>Checkout</button>
                 </Link>
             </div>
--- src/Components/OrderCard/index.jsx
+++ src/Components/OrderCard/index.jsx
@@ -1,4 +1,4 @@
-import {  TrashIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/solid";
+import {  TrashIcon, MinusIcon, PlusIcon } from "@heroicons/react/24/outline";
 
 const OrderCard = props => {
     /**
@@ -10,40 +10,54 @@ const OrderCard = props => {
      * @param {string} props.imageUrl - The URL of the image for the order.
      * @param {number} props.quantity - The quantity of the order.
      */
-    const {id, title, price, imageUrl, quantity, handleDelete, handleChange } = props;
+    const {id, title, price, imageUrl, quantity, handleDelete, handleChange, category } = props;
 
-    let renderDeleteIcon, renderChangeIcon
+    let renderChangeIcon
     
-    if (handleDelete) {
-        renderDeleteIcon =  <TrashIcon className="h-6 w-6 cursor-pointer text-gray-400 hover:text-gray-600" onClick={() => handleDelete(id)} />
-    }
-
     if (handleChange) {         
         renderChangeIcon =
             <div className="relative flex items-center max-w-[8rem]">
-                <button className="flex items-center justify-center p-4 h-8 bg-gray-800 hover:bg-gray-700 rounded-s-sm " type="button" id="decrement-button" >
-                    <MinusIcon className="w-4 h-4 text-white stroke-current stroke-2"/>
+                <button
+                    onClick={() => quantity == 1 ? handleDelete(id) : lessCount(quantity) } 
+                    className="flex items-center justify-center p-4 h-8 bg-gray-800 hover:bg-gray-700 rounded-s-sm " type="button" id="decrement-button" >
+                   {
+                     quantity === 1 
+                        ? <TrashIcon className="w-4 h-4 text-white stroke-current stroke-2"/>
+                        : <MinusIcon className="w-4 h-4 text-white stroke-current stroke-2"/>
+                   }
                 </button>
                 <div 
                     className="flex w-full h-8 px-2 items-center justify-center bg-gray-800 text-center text-white" type="text" id="quantity-input">
-                 3
+                 { quantity }
                 </div>
-                <button className="flex items-center justify-center p-4 h-8 bg-gray-800 hover:bg-gray-700 rounded-e-sm" type="button" id="increment-button"  >
+                <button
+                    onClick={() => addCount(quantity)} 
+                    className="flex items-center justify-center p-4 h-8 bg-gray-800 hover:bg-gray-700 rounded-e-sm" type="button" id="increment-button"  >
                     <PlusIcon className="w-4 h-4 text-white stroke-current stroke-2"  />
                 </button> 
             </div>
     }
 
+    const addCount = (value) => {
+        handleChange(id, value + 1)
+        
+    }
+    
+    const lessCount = (value) => {
+        handleChange(id, value - 1)
+    } 
+
+
     return (
-        <div className="flex my-2 h-32 justify-between items-center ">
+        <div className="flex my-1 h-32 justify-between items-center ">
             <div className="flex items-center justify-between gap-2 h-full">
                 <figure className="w-28 h-28">
                     <img className="w-full h-full rounded-sm object-cover" src={imageUrl} alt={title} />
                 </figure>
-                <div className="flex flex-col justify-between h-full py-2">
+                <div className="flex flex-col justify-between h-full py-2 px-3">
                     <div className="flex flex-col gap-1">
-                        <p className="text-sm font-light">{ title }</p>
-                        <p className="text-sm font-light">{ title }</p>
+                        <p className="text-sm">{ title }</p>
+                        <p className="text-sm font-light">{ category }</p>
                     </div>
                     
                     {renderChangeIcon}
@@ -51,7 +65,7 @@ const OrderCard = props => {
                 
             </div>
             <div className="flex flex-col justify-end items-center h-full py-2">
-                <p className="text-lg font-medium">${ price }</p>
+                <p className="text-lg font-medium">${ quantity * price }</p>
             </div>
         </div>
     )
